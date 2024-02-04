35ba7aff1ce72d968d2907431e311512fbb4e05b
2auto <deutschlei47@126.com>
2024-02-04 14:21:23 +0800

    加入神经模糊PID控制

--- neural_pid/neural_pid2.py
+++ neural_pid/neural_pid2.py
@@ -2,7 +2,7 @@
 Author: zhangL2auto
 Date: 2024-01-23 14:47:30
 LastEditors: 2auto deutschlei47@126.com
-LastEditTime: 2024-01-26 15:40:53
+LastEditTime: 2024-02-04 14:12:51
 '''
 import numpy as np
 import matplotlib.pyplot as plt
@@ -15,10 +15,10 @@ class IncrementalPID:
         self.hiddenLayer_num = 5
         self.outputLayer_num = 3
         self.wi = np.mat([[-0.6394, -0.2696, -0.3756, -0.7023],
-                         [-0.8603, -0.2013, -0.5024, -0.2596],
-                         [-1.0000, 0.5543, -1.0000, -0.5437],
-                         [-0.3625, -0.0724, 0.6463, -0.2859],
-                         [0.1425, 0.0279, -0.5406, -0.7660]])
+                          [-0.8603, -0.2013, -0.5024, -0.2596],
+                          [-1.0000, 0.5543, -1.0000, -0.5437],
+                          [-0.3625, -0.0724, 0.6463, -0.2859],
+                          [0.1425, 0.0279, -0.5406, -0.7660]])
         
         self.wo = np.mat([[0.7576, 0.2616, 0.5820, -0.1416, -0.1325],
                           [-0.1146, 0.2949, 0.8352, 0.2205, 0.4508],
@@ -26,10 +26,8 @@ class IncrementalPID:
         
         self.wi_1 = self.wi
         self.wi_2 = self.wi
-        self.wi_3 = self.wi
         self.wo_1 = self.wo
         self.wo_2 = self.wo
-        self.wo_3 = self.wo
 
         self.Kp = 0.0
         self.Ki = 0.0
@@ -40,7 +38,6 @@ class IncrementalPID:
 
         self.y_out = 0.0    # 系统输出值
         self.y_1 = 0.0      # 上次系统输出值
-        self.y_2 = 0.0      # 上两次系统输出值
 
         self.e = 0.0        # 输出值与输入值的偏差
         self.e_1 = 0.0
@@ -50,9 +47,6 @@ class IncrementalPID:
         self.u = 0.0
         self.u_1 = 0.0
         self.u_2 = 0.0
-        self.u_3 = 0.0
-        self.u_4 = 0.0
-        self.u_5 = 0.0
 
         self.hiddenLayer_input = np.mat(np.zeros((self.hiddenLayer_num, 1)))   # 隐含层的输入
         self.hiddenLayer_output = np.mat(np.zeros((self.hiddenLayer_num, 1)))  # 隐含层的输出
@@ -60,9 +54,9 @@ class IncrementalPID:
         self.hiddenLayer_output_sub = [0.0] * 5
         self.outputLayer_output_sub = [0.0] * 3
         self.outputLayer_d_sub = [0.0] * 3
-        self.temp_4_sub = [0.0] * 3
-        self.dO_sub = [0.0] * 5
-        self.delta2_sub = [0.0] * 5
+        self.outputLayer_delta_sub = [0.0] * 3
+        self.hiddenLayer_d_sub = [0.0] * 5
+        self.hiddenLayer_delta_sub = [0.0] * 5
 
         # 使用den, num模拟系统的实际输出
         self.den = -0.8251
@@ -70,8 +64,7 @@ class IncrementalPID:
     # 设置PID控制器参数
     def SetStepSignal(self, SetSignal):
         # 系统的实际输出值
-        #TODO: Why using u_5
-        self.y_out = -self.den * self.y_1 + self.num * self.u_5
+        self.y_out = -self.den * self.y_1 + self.num * self.u_2
         # 系统的控制误差值
         self.e = SetSignal - self.y_out
         # 神经网络的输入
@@ -133,65 +126,59 @@ class IncrementalPID:
         
         # 链式法则的前四项乘积
         for i in range(self.outputLayer_num):
-            self.temp_4_sub[i] = self.e * self.dyu * self.error_pid.tolist()[i][0] * self.outputLayer_d[0,i]
-        self.temp_4 = np.mat([self.temp_4_sub[0], self.temp_4_sub[1], self.temp_4_sub[2]])
+            self.outputLayer_delta_sub[i] = self.e * self.dyu * self.error_pid.tolist()[i][0] * self.outputLayer_d[0,i]
+        self.outputLayer_delta = np.mat(self.outputLayer_delta_sub)
 
         for l in range(self.outputLayer_num):
-            for i in range(self.inputLayer_num):
-                self.d_wo = (1 - self.alfa) * self.lr * self.temp_4_sub[l] * self.hiddenLayer_output.tolist()[i][0] + self.alfa * (self.wo_1 - self.wo_2)
-                # self.d_wo = self.lr * self.temp_4_sub[l] * self.hiddenLayer_output.tolist()[i][0] + self.alfa * (self.wo_1 - self.wo_2)
+            for i in range(self.hiddenLayer_num):
+                self.d_wo = (1 - self.alfa) * self.lr * self.outputLayer_delta_sub[l] * self.hiddenLayer_output.tolist()[i][0] + self.alfa * (self.wo_1 - self.wo_2)
+                # self.d_wo = self.lr * self.outputLayer_delta_sub[l] * self.hiddenLayer_output.tolist()[i][0] + self.alfa * (self.wo_1 - self.wo_2)
 
         # self.wo = self.wo_1 + self.d_wo + self.alfa * (self.wo_1 - self.wo_2)
         self.wo = self.wo_1 + self.d_wo
+        
+        # 中间层激活函数求导
+        for i in range(self.hiddenLayer_num):
+            hidden = self.hiddenLayer_input.tolist()[0][i]
+            self.hiddenLayer_d_sub[i] = 4 / ((np.exp(hidden) + np.exp(-hidden)) * (np.exp(hidden) + np.exp(-hidden)))
+        self.hiddenLayer_d = np.mat(self.hiddenLayer_d_sub)
+        # 中间层权重学习
+        self.hiddeLayer_delta_temp = np.dot(self.outputLayer_delta, self.wo)
+        # 中间层激活函数导数 * 中间层权重学习
+        for i in range(self.hiddenLayer_num):
+             self.hiddenLayer_delta_sub[i] = self.hiddenLayer_d_sub[i] * self.hiddeLayer_delta_temp.tolist()[0][i]
+        self.hiddenLayer_delta = np.mat(self.hiddenLayer_delta_sub)
 
-        for i6 in range(5):
-            self.dO_sub[i6] = 4 / ((np.e ** (self.hiddenLayer_input.tolist()[0][i6]) + np.e ** (-self.hiddenLayer_input.tolist()[0][i6])) * (np.e ** (self.hiddenLayer_input.tolist()[0][i6]) + np.e ** (-self.hiddenLayer_input.tolist()[0][i6])))
-        self.dO = np.mat([self.dO_sub[0], self.dO_sub[1], self.dO_sub[2], self.dO_sub[3], self.dO_sub[4]])
-
-        self.segma = np.dot(self.temp_4, self.wo)
-
-        for i7 in range(5):
-             self.delta2_sub[i7] = self.dO_sub[i7] * self.segma.tolist()[0][i7]
-        self.delta2 = np.mat([self.delta2_sub[0], self.delta2_sub[1], self.delta2_sub[2], self.delta2_sub[3], self.delta2_sub[4]])
-
-        self.d_wi = (1 - self.alfa) * self.lr * self.delta2.T * self.r_in + self.alfa * (self.wi_1 - self.wi_2)
+        self.d_wi = (1 - self.alfa) * self.lr * self.hiddenLayer_delta.T * self.r_in + self.alfa * (self.wi_1 - self.wi_2)
         self.wi = self.wi_1 + self.d_wi
 
         # 参数更新
-        self.u_5 = self.u_4
-        self.u_4 = self.u_3
-        self.u_3 = self.u_2
         self.u_2 = self.u_1
         self.u_1 = self.u
 
-        self.y_2 = self.y_1
         self.y_1 = self.y_out
 
-        self.wo_3 = self.wo_2
         self.wo_2 = self.wo_1
         self.wo_1 = self.wo
-
-        self.wi_3 = self.wi_2
         self.wi_2 = self.wi_1
         self.wi_1 = self.wi
 
         self.e_2 = self.e_1
         self.e_1 = self.e
+        
         self.lr_1 = self.lr
-
 
         # IncrementValue = self.Kp * (self.Error - self.LastError) + self.Ki * self.Error + self.Kd * (
         #             self.Error - 2 * self.LastError + self.LastLastError)
         # self.PIDOutput += IncrementValue
         # self.LastLastError = self.LastError
         # self.LastError = self.Error
-
     # 设置一阶惯性环节系统  其中InertiaTime为惯性时间常数
     def SetInertiaTime(self, InertiaTime, SampleTime):
-        self.y_out = (InertiaTime * self.y_1 + SampleTime * self.u_5) / (SampleTime + InertiaTime)
+        self.y_out = (InertiaTime * self.y_1 + SampleTime * self.u_2) / (SampleTime + InertiaTime)
         # self.LastSystemOutput = self.SystemOutput
 
-plt.figure(1)  # 创建图表1
+plt.figure("neural pid")  # 创建图表1
 
 def TestPID():
     Process = IncrementalPID()
@@ -200,7 +187,7 @@ def TestPID():
     X = [0]
     Y_out = [0]
 
-    for i in range(1, 500):
+    for i in range(1, 100):
         Process.SetStepSignal(10)
         # Process.SetInertiaTime(100, 50)
         ProcessXaxis.append(i)
@@ -208,7 +195,8 @@ def TestPID():
         X.append(i)
         Y_out.append(Process.e)
         plt.plot(ProcessXaxis, ProcessYaxis, 'r')
-        plt.plot(ProcessXaxis, np.ones(len(ProcessXaxis))*10, 'g')
+        plt.plot(ProcessXaxis, np.ones(len(ProcessXaxis))*600, 'g')
+        plt.text(x = 10, y = 10, s = Process.Kp)
         plt.pause(0.01)
     plt.show()
 
